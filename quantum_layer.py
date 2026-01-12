019c482687b1783a86c0f801660ac52c63a8627e
CodexPromptusIuris <matiasrojasf02@gmail.com>
2026-01-12 03:38:15 -0300

    Actualizar quantum_layer.py

--- quantum_layer.py
+++ quantum_layer.py
@@ -1,26 +1,20 @@
 import pennylane as qml
-import tensorflow as tf
+import pennylane.numpy as np
 
 NUM_QUBITS = 4
 dev = qml.device("default.qubit", wires=NUM_QUBITS)
 
-@qml.qnode(dev, interface="tf")
+@qml.qnode(dev, interface="autograd")
 def quantum_circuit(inputs, weights):
+    # inputs shape: (4,)
     for i in range(NUM_QUBITS):
         qml.RY(inputs[i], wires=i)
 
+    # weights shape: (4,)
     for i in range(NUM_QUBITS):
         qml.RX(weights[i], wires=i)
 
     for i in range(NUM_QUBITS - 1):
         qml.CNOT(wires=[i, i + 1])
 
-    return [qml.expval(qml.PauliZ(i)) for i in range(NUM_QUBITS)]
-
-weight_shapes = {"weights": (NUM_QUBITS,)}
-
-quantum_layer = qml.qnn.KerasLayer(
-    quantum_circuit,
-    weight_shapes,
-    output_dim=NUM_QUBITS,
-)
+    return np.array([qml.expval(qml.PauliZ(i)) for i in range(NUM_QUBITS)])
