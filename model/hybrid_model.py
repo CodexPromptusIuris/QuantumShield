f2aacd1a4aee8dd8485839216c74190765958bd7
CodexPromptusIuris <matiasrojasf02@gmail.com>
2026-01-12 02:12:48 -0300

    Crear model/hybrid_model.py

---
+++ model/hybrid_model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from quantum.quantum_layer import quantum_layer, NUM_QUBITS
+
+def build_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(NUM_QUBITS,)),
+        quantum_layer,
+        tf.keras.layers.Dense(8, activation="relu"),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="binary_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
